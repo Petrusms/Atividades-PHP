95d84c9dcc32a6aadb4ade678db7c3826ca80b5b
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-10-17 15:12:58 -0300

    Update execucao.php

--- Orientação Objeto/FormasGeo/execucao.php
+++ Orientação Objeto/FormasGeo/execucao.php
@@ -10,27 +10,18 @@ $type = readline("");
 if($type === "Qua"){
     $Quadrado = new quadrado();
     $Quadrado->setLado(readline("Qual o valor do lado desse quadrado?\n"));
-    $area = $Quadrado->getArea();
-    $desenho = $Quadrado->getDesenho();
-
-    print("O resultado da área é igual a $area\n");
-    print($desenho);
+    print("O resultado da área é igual a ".$Quadrado->getArea()."\n");
+    print($Quadrado->getDesenho());
 }else if($type === "Cir"){
     $Circulo = new circulo();
     $Circulo->setRaio(readline("Qual o valor do raio desse círculo?\n"));
-    $area = $Circulo->getArea();
-    $desenho = $Circulo->getDesenho();
-
-    print("O resultado da área é igual a $area\n");
-    print($desenho);
+    print("O resultado da área é igual a ".$Circulo->getArea()."\n");
+    print($Circulo->getDesenho());
 }else{
     $Retangulo = new retangulo();
     $Retangulo->setAltura(readline("Qual o valor da altura desse retângulo?\n"));
     $Retangulo->setBase(readline("Qual o valor da altura desse retângulo?\n"));
-    $area = $Retangulo->getArea();
-    $desenho = $Retangulo->getDesenho();
-
-    print("O resultado da área é igual a $area\n");
+    print("O resultado da área é igual a ".$Retangulo->getArea()."\n");
     print($desenho);
 }
 ?>
