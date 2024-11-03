21cb940540878d7d968e9b059a4aba8fb6ac0b78
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-11-03 11:44:25 -0300

    Update CarroPasseio.php

--- Orientação Objeto/Herença/Automoveis/modelo/CarroPasseio.php
+++ Orientação Objeto/Herença/Automoveis/modelo/CarroPasseio.php
@@ -1,29 +1,48 @@
 <?php
-    require_once("Automoveis.php");
-    class CarroPasseio extends Automoveis{
-        private int $qtdPassageiros;
+require_once("Automoveis.php");
+class Onibus extends Automoveis{
+    private int $qtdPassageiros;
+    private int $pesoMax;
 
-        public function getDados(){
-            return "Modelo: ".$this->modelo."\nVelocidade Máxima: ".$this->velocidadeMax.
-                    "Km/h\nCor: ".$this->cor."\nQuantidade de Passageiros: ".$this->getQtdPassageiros()."\n";
-        }
-
-        /**
-         * Get the value of qtdPassageiros
-         */
-        public function getQtdPassageiros(): int
-        {
-                return $this->qtdPassageiros;
-        }
-
-        /**
-         * Set the value of qtdPassageiros
-         */
-        public function setQtdPassageiros(int $qtdPassageiros): self
-        {
-                $this->qtdPassageiros = $qtdPassageiros;
-
-                return $this;
-        }
+    public function getDados(){
+        return "Modelo: ".$this->modelo."\nVelocidade Máxima: ".$this->velocidadeMax.
+                "Km/h\nCor: ".$this->cor."\nQuantidade de Passageiros: ".$this->getQtdPassageiros()."\nPeso máximo: ".$this->getPesoMax()."Kg\n";
     }
+
+    /**
+     * Get the value of qtdPassageiros
+     */
+    public function getQtdPassageiros(): int
+    {
+        return $this->qtdPassageiros;
+    }
+
+    /**
+     * Set the value of qtdPassageiros
+     */
+    public function setQtdPassageiros(int $qtdPassageiros): self
+    {
+        $this->qtdPassageiros = $qtdPassageiros;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pesoMax
+     */
+    public function getPesoMax(): int
+    {
+        return $this->pesoMax;
+    }
+
+    /**
+     * Set the value of pesoMax
+     */
+    public function setPesoMax(int $pesoMax): self
+    {
+        $this->pesoMax = $pesoMax;
+
+        return $this;
+    }
+}
 ?>
