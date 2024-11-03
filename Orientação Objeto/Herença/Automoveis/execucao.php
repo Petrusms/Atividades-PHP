5cac91896cd8564ca8a8040c515f924db1d9dafc
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-11-03 11:43:34 -0300

    Update execucao.php

--- Orientação Objeto/Herença/Automoveis/execucao.php
+++ Orientação Objeto/Herença/Automoveis/execucao.php
@@ -3,7 +3,17 @@
     require_once("modelo/CarroPasseio.php");
     require_once("modelo/Onibus.php");
 
-    $carroP = new CarroPasseio("Civic", "Prateado", "200Km");
-    $carroP->setQtdPassageiros("5");
-    print($carro->getDados);
+    $carroP = new CarroPasseio("Civic", "Prateado", 200);
+    $carroP->setQtdPassageiros(5);
+    print($carroP->getDados());
+
+    $onibus = new Onibus("Mercedes-Benz", "Azul", 108);
+    $onibus->setQtdPassageiros(44);
+    $onibus->setPesoMax(16.500);
+    print($onibus->getDados());
+
+    $caminhao = new Caminhao("Caterpillar", "verde", 68);
+    $caminhao->setQtdCarga(400);
+    $caminhao->setAltura(6,52);
+    print($caminhao->getDados());
     ?>
