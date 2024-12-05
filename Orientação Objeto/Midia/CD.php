14cefd721b48f7041de06f85bc93f644f9fcf1c3
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-12-05 15:45:52 -0300

    Update CD.php

--- Orientação Objeto/Midia/CD.php
+++ Orientação Objeto/Midia/CD.php
@@ -2,7 +2,7 @@
 require_once("Midia.php");
 class CD extends Midia{
     public function getTipo($midia){
-                return "CD | Descrição: ".$midia->getDescricao()." | Preço: ".$midia->getPrecoPago()."\n";
+                return "CD | ".$midia->getdados()."\n";
     }
 }
 ?>
