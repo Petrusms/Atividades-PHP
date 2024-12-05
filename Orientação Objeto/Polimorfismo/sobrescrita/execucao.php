cd19dd054416d2860ac2bce366af80a41d3393b5
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-12-05 14:07:18 -0300

    Update execucao.php

--- Orientação Objeto/Polimorfismo/sobrescrita/execucao.php
+++ Orientação Objeto/Polimorfismo/sobrescrita/execucao.php
@@ -1,11 +1,17 @@
 <?php
 require_once("modelo/caneta.php");
 require_once("modelo/canetaAzul.php");
+require_once("modelo/canetaVermelha.php");
 
 $c = new caneta();
 $c->escrever();
 
 $c = new canetaAzul();
 $c->escrever();
+
+$c = new canetaVermelha();
+$c->escrever();
+$c->escreverEspec();
+$c->escreverPai();
 
 ?>
