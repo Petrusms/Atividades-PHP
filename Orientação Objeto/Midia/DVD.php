75675446c274d35b6518bc43fe278f1569f9b2a6
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-12-07 23:03:03 -0300

    Update DVD.php

--- Orientação Objeto/Midia/DVD.php
+++ Orientação Objeto/Midia/DVD.php
@@ -1,8 +1,8 @@
 <?php
 require_once("Midia.php");
 class DVD extends Midia{
-    public function getTipo($midia){
-        return "DVD | ".$midia->getdados()."\n";
+    public function getTipo(){
+        return "DVD | ";
     }
 }
 ?>
