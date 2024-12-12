eca8a74f152d184c572e3fa00378f7bbed8bcb02
Petrusms <134728686+Petrusms@users.noreply.github.com>
2024-12-12 14:07:37 -0300

    Add files via upload

---
+++ Orientação Objeto/Midia/Class Abstract/modelo/cd.php
@@ -0,0 +1,8 @@
+<?php
+require_once("midia.php");
+class cd extends midia{
+    public function getTipo(){
+        return "CD";
+    }
+}
+?>
---
+++ Orientação Objeto/Midia/Class Abstract/modelo/dvd.php
@@ -0,0 +1,8 @@
+<?php
+require_once("midia.php");
+class dvd extends midia{
+    public function getTipo(){
+        return "DVD";
+    }
+}
+?>
